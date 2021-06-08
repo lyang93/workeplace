1664f3a6e3cc4d976d2dc54aad8e12eb5d1ad3f9
lyang93 <275534010@qq.com>
2021-06-08 21:20:13 +0800

    first commit

---
+++ cloud2020/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int i = paymentService.create(payment);
+        if(i >0){
+            log.info("插入数据成功，{}",i);
+            return new CommonResult(200,"插入数据成功,serverPort:"+serverPort,i);
+        }
+        else {
+            log.info("插入数据失败");
+            return new CommonResult(444,"插入数据失败,serverPort:"+serverPort,null);
+        }
+    }
+
+    @GetMapping(value = "/payment/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("查询结果：{}",payment);
+        return payment!=null?new CommonResult(200,"查询数据成功,serverPort:"+serverPort,payment):
+                new CommonResult(444,"数据不有存在，id:"+id+",serverPort:"+serverPort);
+    }
+}
